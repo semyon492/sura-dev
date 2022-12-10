9a578a48c406e61b3700d0e16892e55f09512c30
Semen Alekseev <semyon492@ya.ru>
2022-12-10 14:37:00 +0300

    Upd

--- public/index.php
+++ public/index.php
@@ -1,3 +1,38 @@
 <?php
+/*
+ * Copyright (c) 2022 Sura
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ *
+ */
 
-phpinfo();
+declare(strict_types=1);
+
+use Mozg\Mozg;
+
+if (\version_compare(PHP_VERSION, '8.1.5') < 0) {
+    throw new \RuntimeException('Please change php version');
+}
+if (isset($_POST['PHPSESSID'])) {
+    \session_id($_POST['PHPSESSID']);
+}
+\session_start();
+\ob_start();
+\ob_implicit_flush(false);
+const ROOT_DIR = __DIR__ . '/../';
+const ENGINE_DIR = ROOT_DIR . '/system';
+try {
+    require __DIR__ . '/../vendor/autoload.php';
+} catch (\Error) {
+    throw new \RuntimeException('Please install composer');
+}
+
+/** Initialize */
+try {
+    (new Mozg())::initialize();
+} catch (JsonException $e) {
+} catch (\Mozg\exception\ErrorException $e) {
+}
+
+
